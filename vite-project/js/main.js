e104ffbb6202d0ee142c6c4353c08ce81ec30ca7
oraclelens <sophiar10200@gmail.com>
2022-12-06 13:15:55 -0500

    hi

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,1 +1,8 @@
 import "../styles/style.css";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
+document.querySelector("#app").innerHTML = `
+<h1> Hello ?????? </h1>
+<a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>`;
