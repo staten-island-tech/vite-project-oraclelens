e2f2b2d484caa955350e9f140a4bbaf7f31d62f5
oraclelens <sophiar10200@gmail.com>
2022-12-09 13:13:54 -0500

    burg

---
+++ vite-project/js/burger.js
@@ -0,0 +1,82 @@
+const burgeroptions = [
+  {
+    name: "beef patty",
+    vegetarian: "no",
+    calories: 204,
+    part: "patty",
+  },
+  {
+    name: "fried chicken",
+    vegetarian: "no",
+    calories: 210,
+    part: "patty",
+  },
+  {
+    name: "fried tofu",
+    vegetarian: "yes",
+    calories: 149,
+    part: "patty",
+  },
+  {
+    name: "veggie patty",
+    vegetarian: "yes",
+    calories: 124,
+    part: "patty",
+  },
+  {
+    name: "cheddar",
+    vegetarian: "yes",
+    calories: 113,
+    part: "cheese",
+  },
+  {
+    name: "monterey jack",
+    vegetarian: "yes",
+    calories: 106,
+    part: "cheese",
+  },
+  {
+    name: "white cheddar",
+    vegetarian: "yes",
+    calories: 80,
+    part: "cheese",
+  },
+  {
+    name: "mozzerella cheese",
+    vegetarian: "yes",
+    calories: 78,
+    part: "cheese",
+  },
+  {
+    name: "lettuce",
+    vegetarian: "yes",
+    calories: 3,
+    part: "toppings",
+  },
+  {
+    name: "tomato",
+    vegetarian: "yes",
+    calories: 8,
+    part: "toppings",
+  },
+  {
+    name: "avocado",
+    vegetarian: "yes",
+    calories: 100,
+    part: "toppings",
+  },
+  {
+    name: "bacon",
+    vegetarian: "no",
+    calories: 86,
+    part: "toppings",
+  },
+];
+
+const products = {
+  getPatty: function () {
+    burgeroptions
+      .filter((el) => el.part === "patty")
+      .forEach((el) => console.log(el.name));
+  },
+};
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,6 +2,7 @@ import "../styles/style.css";
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
+import "../js/burger.js";
 AOS.init();
 document.querySelector("#app").innerHTML = `
 <h1 class=title>Build-A-Burger</h1>`;
