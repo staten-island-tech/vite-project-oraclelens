48d37bd98e6330eb96163e466bcd6643db0a407e
oraclelens <sophiar10200@gmail.com>
2022-12-15 13:14:35 -0500

    background

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,4 +1,5 @@
 const DOMSelectors = {
+  button: document.getElementById("darkmode"),
   button1: document.getElementById("menuall"),
   button2: document.getElementById("patty"),
   button3: document.getElementById("cheese"),
@@ -8,3 +9,13 @@ const DOMSelectors = {
 };
 
 export { DOMSelectors };
+
+DOMSelectors.button.addEventListener("click", function () {
+  if (document.body.classList.contains("light")) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+});
